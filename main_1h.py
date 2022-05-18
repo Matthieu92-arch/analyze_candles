f1680df1a6c309aaa808c6515a175d2c1a60f857
Matthieu W <matthieu.werth@deepki.com>
2022-05-18 14:04:28 +0200

    Add 1H

--- main_1h.py
+++ main_1h.py
@@ -1,7 +1,12 @@
+import pickle
+from pathlib import Path
+from time import sleep
+
 import pandas as pd
 
 from ftx.rest.client import FtxClient
 from process import check_sfp
+from save_datas import read_list_from_file, write_list_to_file
 
 api_key = "Dl3Px24FKRjEs18ObF06uwB9GGpss9-q-IMyofzO"
 api_secret = "LUqdaDI9U9kUlFXFq1G9iYFcVnCfPP1PmkQaxUH1"
@@ -10,7 +15,9 @@ api_secret = "LUqdaDI9U9kUlFXFq1G9iYFcVnCfPP1PmkQaxUH1"
 # client = FtxClient(api_key, api_secret)
 client = FtxClient(api_key, api_secret, 'trading')
 
-markets = ['BTC-PERP', 'ETH-PERP', 'FTM-PERP', 'ATOM-PERP', 'SOL-PERP', 'LTC-PERP', 'DOT-PERP', 'ETH-PERP', 'MATIC-PERP']
+markets = ['BTC-PERP', 'ETH-PERP', 'FTM-PERP', 'ATOM-PERP', 'SOL-PERP',
+           'LTC-PERP', 'DOT-PERP', 'ETH-PERP', 'MATIC-PERP']
+# markets = ['ETH-PERP']
 seconds_time = 60
 
 
@@ -18,18 +25,29 @@ def get_candles(market, minutes):
     return client.get_historical_prices(market, (minutes * 60))
 
 
-def loop_candles(df_candles, market, min):
-    for i in range(1, 2):
-        check_sfp(market, df_candles, i, min)
-
-
 def launch_surveillance():
+    # sleep(10)
+    far_candle = 50
+    file_name = 'output_1h.txt'
+    hash_alerts = read_list_from_file(file_name)
+
+    print("\n\nChecking Sfp in market list...\n")
     for market in markets:
         df_candles = get_candles(market, seconds_time)
         df_candles = pd.DataFrame.from_records(df_candles)
         df_candles = df_candles[::-1].reset_index()
         print("Data obtained.")
-        loop_candles(df_candles, market, 60)
+        hash_alerts = loop_candles(df_candles, market, 1, far_candle, hash_alerts)
+
+    write_list_to_file(file_name, hash_alerts)
+    print('Done')
+
+
+def loop_candles(df_candles, market, min, far_candle, hash_alerts):
+    print(market)
+    for i in range(1, 1200):
+        hash_alerts = check_sfp(market, df_candles, i, min, far_candle, hash_alerts)
+    return hash_alerts
 
 
 launch_surveillance()
